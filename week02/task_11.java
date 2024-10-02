36e6c871a2ecdc32a2fd7447ab6b9e1cea7397fe
hyering126 <hyering126@naver.com>
2024-10-02 18:51:24 +0900

    update body
    added div

---
+++ week02/task_11.java
@@ -0,0 +1,10 @@
+package week02;
+
+public class task_11 {
+
+    public static void main(String[] args) {
+        for (int i = 0, j = 10; i <= 5 && j <= 10; i++, j--) {
+            System.out.println("i=" + i + "j=" + j);
+        }
+    }
+}
