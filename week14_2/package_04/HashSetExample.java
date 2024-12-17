5ea4e5382654d088ee9027fbd971b13ba6b772cd
hyering126 <127617801+hyering126@users.noreply.github.com>
2024-12-17 23:21:11 +0900

    Add files via upload

---
+++ week14_2/package_04/HashSetExample.java
@@ -0,0 +1,24 @@
+package package_04;
+
+import java.util.*;
+
+public class HashSetExample {
+
+	public static void main(String[] args) {
+		// HashSet 컬렉션 생성
+		Set<String> set = new HashSet<String>();
+
+		// 객체 저장
+		set.add("Java");
+		set.add("JDBC");
+		set.add("Servlet/JSP");
+		set.add("Java"); // <- 중복 객체이므로 저장하지 않음
+		set.add("iBATIS");
+
+		// 저장된 객채 수 출력
+		int size = set.size();
+		System.out.println("총 객체 수: " + size);
+
+	}
+
+}
